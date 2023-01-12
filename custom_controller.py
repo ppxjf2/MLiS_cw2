a1a0f4a1d19c20af60b4fd5a995c2e2a6aa9a6ba
SDMcSweeney <ppxsm5@nottingham.ac.uk>
2023-01-12 16:43:50 +0000

    Update custom_controller.py
    reward function

--- custom_controller.py
+++ custom_controller.py
@@ -2,9 +2,14 @@ import numpy as np
 from flight_controller import FlightController
 from drone import Drone
 from typing import Tuple
+from math import log
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
+
+
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
 
 class CustomController(FlightController):
 
@@ -21,7 +26,10 @@ class CustomController(FlightController):
         self.states = []
         self.actions = []
         self.rewards = []
-        self.value_function = np.zeros((20,20,20,5))
+        values= np.zeros(20,20,20)
+        actions = (np.array([50,4,3,2,1]))
+        values = [values,actions]
+        self.value_function = values
 
     def value_calc(self, state):
        
@@ -45,6 +53,38 @@ class CustomController(FlightController):
     def b(self, x):
         i = x + self.h
         return sigmoid(i)
+
+    def reward(self, drone: Drone):
+
+        target_point = drone.get_next_target()
+        dx = target_point[0] - drone.x
+        dy = target_point[1] - drone.y
+
+        dist = np.sqrt(np.square(dx) + np.square(dy))
+        velocity = np.sqrt(np.square(drone.velocity_x) + np.square(drone.velocity_y))
+
+        if (dist<self.h1):
+            ontarget= True
+        else:
+            ontarget = False
+
+
+        if velocity < 0.05:
+            velocity_multiplier = 0
+        else:
+            velocity_multiplier= clamp(-10(velocity-0.1)**2+1 ,0,1)
+
+        if drone.pitch == 0:
+            angle_multiplier = 1
+        else:
+            angle_multiplier= clamp(-log(30*abs(drone.pitch)) ,0,1)
+        
+        rotational_multiplier = clamp( -(drone.pitch_velocity/20)**2+1,0,1)
+
+        multiplier =  velocity_multiplier*angle_multiplier* rotational_multiplier
+
+        reward = -dist + ontarget(10*multiplier)
+        return reward
 
     def get_thrusts(self, drone: Drone) -> Tuple[float, float]:
 
